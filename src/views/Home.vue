57023d2c5ce2d9968a6c2fd4b322191a79b12a7c
Shahin <shahin@uilicious.com>
2018-10-14 16:48:15 +0800

    Home page header text size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,14 @@
         <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
             <v-layout align-center fill-height class="white--text">
                 <v-flex text-xs-center>
-                    <h3 class="display-3">Welcome to Sopnopriyo</h3>
-                    <div class="body-1">Thanks ! This is one of the best place to know more about Shahin. keep
-                        exploring !</div>
-
+                    <h1 class="display-3">Welcome to Sopnopriyo</h1>
+                    <div class="display-1">Thanks ! This is one of the best place to know more about Shahin. keep
+                        exploring !
+						</div>
                 </v-flex>
             </v-layout>
         </v-parallax>
         <v-container>
-            Some text
         </v-container>
     </v-layout>
 </template>
