36e9b88d3179eca97890c0d6fc640e9368d13212
Shahin <shahin@uilicious.com>
2018-10-15 23:10:20 +0800

    Added meaningful header with important keywords in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,7 @@
         <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
             <v-layout align-center fill-height class="white--text">
                 <v-flex text-xs-center>
-                    <h1 class="display-3">Welcome to Sopnopriyo</h1>
-                    <div class="display-1">Thanks ! This is one of the best place to know more about Shahin. keep
-                        exploring !
-						</div>
+                    <h1 class="display-2">Full stack Developer known as Sopnopriyo from Singapore</h1>
                 </v-flex>
             </v-layout>
         </v-parallax>
