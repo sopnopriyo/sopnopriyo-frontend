1fe1b7bdb347131dd5be51e09390437e7e9a4a64
Shahin <shahin@uilicious.com>
2018-10-17 23:41:43 +0800

    Updated contact me page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,24 +1,31 @@
 <template>
     <v-container fluid grid-list-md>
         <h1 class="mt-5 text-md-center">Contact Me</h1>
-            <v-layout align-center justify-center row wrap>
-                <v-flex xs12 sm12 md4>
-                    <v-text-field name="name" v-model="contact.name" :rules="nameRules" label="Name" required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm12 md4>
-                    <v-text-field name="email" v-model="contact.email" :rules="emailRules" label="E-mail" required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm12 md8>
-                    <v-text-field name="subject" v-model="contact.subject" :rules="subjectRules" label="Subject"
-                        required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm12 md8>
-                    <v-textarea name="body" label="Body" :rules="bodyRules"></v-textarea>
-                </v-flex>
-                <v-flex xs12 sm12 md8>
-                    <v-btn primary large block>Send</v-btn>
-                </v-flex>
-            </v-layout>
+		
+        <v-layout align-center justify-center row wrap>
+            <v-card hover>
+                <v-container fill-height fluid>
+					        <v-layout align-center justify-center row wrap>
+                    <v-flex xs12 sm12 md6>
+                        <v-text-field name="name" v-model="contact.name" :rules="nameRules" label="Name" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm12 md6>
+                        <v-text-field name="email" v-model="contact.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm12 md12>
+                        <v-text-field name="subject" v-model="contact.subject" :rules="subjectRules" label="Subject"
+                            required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm12 md12>
+                        <v-textarea name="body" label="Body" :rules="bodyRules"></v-textarea>
+                    </v-flex>
+                    <v-flex xs12 sm12 md2>
+                        <v-btn primary medium block>Send</v-btn>
+                    </v-flex>
+					        </v-layout>
+                </v-container>
+            </v-card>
+        </v-layout>
     </v-container>
 </template>
 <style lang="scss">
