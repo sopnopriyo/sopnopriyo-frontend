f31b5ed311cfb44bd8f3c278cebf8daf67556d9f
Shahin Alam <sopnopriyo@gmail.com>
2019-11-10 13:43:29 +0800

    Updated build

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -8,11 +8,8 @@ import {
 
 import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\assets\\css\\main.scss'
-
-import '..\\node_modules\\vuetify\\dist\\vuetify.css'
 
 import _1bffa850 from '..\\layouts\\admin-panel.vue'
 import _6f6c098b from '..\\layouts\\default.vue'
@@ -63,7 +60,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -173,10 +170,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
